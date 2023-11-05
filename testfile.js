e5ec19a222018295e00bb58c6d12ff79c1432c4b
Manav Khandurie <manavkhandurie@gmail.com>
2023-11-05 16:19:59 +0530

    Image uploaded

---
+++ testfile.js
@@ -0,0 +1,3 @@
+cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" }, 
+  function(error, result) {console.log(result); });
